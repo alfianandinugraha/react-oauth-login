11666eb69bf7ff1a76ab97122c948f9d43ed4d99
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-12 11:27:26 +0700

    feat(pages): init design Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
+import BaseLayout from '@/layouts/BaseLayout'
+import { Typography } from '@mui/material'
 import React from 'react'
 
 const HomePage = (): React.ReactElement => {
-  return <h1>Home page</h1>
+  return (
+    <BaseLayout>
+      <Typography variant="h4">Home</Typography>
+    </BaseLayout>
+  )
 }
 
 export default HomePage
