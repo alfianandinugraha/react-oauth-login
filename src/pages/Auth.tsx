91b8ccee3d571a65fddfdd691f21d0a87e341f1f
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-12 11:57:27 +0700

    refactor(file): remove .env file

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -4,11 +4,28 @@ import { Facebook, GitHub, Google } from '@mui/icons-material'
 import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
 
+const FACEBOOK_APP_ID = 308645417927319
+
+const REDIRECT_URI = 'http://localhost:3000/oauth'
+
 const OAuthButton = styled(Button)`
   margin-bottom: 16px;
 `
 
 const AuthPage = (): React.ReactElement => {
+  const openDialog = (url: string) => {
+    window.open(url, '', 'width=700, height=700,fullscreen=no')
+  }
+
+  const facebookDialog = () => {
+    const state = JSON.stringify({
+      vendor: 'facebook',
+    })
+    openDialog(
+      `https://www.facebook.com/v12.0/dialog/oauth?client_id=${FACEBOOK_APP_ID}&redirect_uri=${REDIRECT_URI}&state=${state}`
+    )
+  }
+
   return (
     <BaseLayout>
       <Typography variant="h4">Login</Typography>
@@ -28,6 +45,7 @@ const AuthPage = (): React.ReactElement => {
               backgroundColor: '#2b4c7a',
             },
           }}
+          onClick={facebookDialog}
         >
           Login with Facebook
         </OAuthButton>
