1bbd222c9653ee7a10f04e659ad6c64eaea98de7
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-13 10:05:04 +0700

    feat(pages): receive response from oauth vendor on OAuth.tsx

--- src/pages/OAuth.tsx
+++ src/pages/OAuth.tsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import { OAuthResponse } from 'types'
+import * as ls from 'local-storage'
 
 const OAuthPage = (): React.ReactElement => {
+  const [searchParams] = useSearchParams()
+
+  useEffect(() => {
+    const state = JSON.parse(searchParams.get('state') || '')
+    const vendor = state.vendor as OAuthResponse['vendor']
+
+    if (vendor === 'facebook') {
+      const error = searchParams.get('error')
+      if (!error) window.close()
+
+      const code = searchParams.get('code') || ''
+      const response: OAuthResponse = {
+        vendor: 'facebook',
+        success: {
+          code,
+        },
+      }
+      ls.set('oauth-response', response)
+    }
+
+    window.close()
+  }, [])
   return <h1>OAuth Page</h1>
 }
 
