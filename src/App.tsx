f2eef87585bc87318b2705b56a5373d5b2b843dd
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-11-13 14:28:46 +0700

    feat(components): add github corner and move toast to top-left

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Toaster } from 'react-hot-toast'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import GithubCorner from './components/GithubCorner'
 import HomePage from './pages/Home'
 import OAuthPage from './pages/OAuth'
 
 const App = (): React.ReactElement => {
   return (
     <>
-      <Toaster position="top-right" />
+      <Toaster position="top-left" />
+      <GithubCorner />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
